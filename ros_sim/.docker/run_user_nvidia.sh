ab29fdea0441558e062b3c3033aec606ebd7865d
huanyu <HuanyuL@users.noreply.github.com>
2024-01-24 11:57:22 +0100

    update dockerfile

--- ros_sim/.docker/run_user_nvidia.sh
+++ ros_sim/.docker/run_user_nvidia.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 echo -e "Starting up ros_sim container \n >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-echo -e "This container will access to the users home directory and log in as the user with their password and x sever access.\nYou will not own the workspace though, use sudo chown -R $USER /pcd_processing to change this."
+echo -e "This container will access to the users home directory and log in as the user with their password and x sever access.\nYou will not own the workspace though, use sudo chown -R $USER /dev_ws to change this."
 
 docker run -it --privileged \
     --user=$(id -u $USER):$(id -g $USER) \
     --group-add sudo \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
-    --workdir="/pcd_processing" \
+    --workdir="/dev_ws" \
     --volume="/home/$USER:/home/$USER" \
     --volume="/etc/group:/etc/group:ro" \
     --volume="/etc/passwd:/etc/passwd:ro" \
