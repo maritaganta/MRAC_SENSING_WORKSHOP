68659e47c703730165721b25b056a31b5dc7f493
huanyu <HuanyuL@users.noreply.github.com>
2024-01-24 11:47:50 +0100

    update nvidia config

--- ros_sim/.docker/run_user_nvidia.sh
+++ ros_sim/.docker/run_user_nvidia.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
 echo -e "Starting up ros_sim container \n >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-echo -e "This container will access to the users home directory and log in as the user with their password and x sever access.\nYou will not own the workspace though, use sudo chown -R $USER /dev_ws"
-echo -e "Source the workspace with source devel/setup.bash"
+echo -e "This container will access to the users home directory and log in as the user with their password and x sever access.\nYou will not own the workspace though, use sudo chown -R $USER /pcd_processing to change this."
 
 docker run -it --privileged \
     --user=$(id -u $USER):$(id -g $USER) \
     --group-add sudo \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
-    --workdir="/dev_ws" \
+    --workdir="/pcd_processing" \
     --volume="/home/$USER:/home/$USER" \
     --volume="/etc/group:/etc/group:ro" \
     --volume="/etc/passwd:/etc/passwd:ro" \
@@ -17,6 +16,7 @@ docker run -it --privileged \
     --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --volume="/dev:/dev" \
+    --runtime=nvidia \
     --net=host \
-    --cap-add=sys_nice \
+    --gpus 'all,"capabilities=compute,display,graphics,utility"' \
     ros_sim:latest
